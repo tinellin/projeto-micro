b5f57d3aa91a9a9dc399a4965f2357921f5ab807
Enzo Tinelli <enzo.tinelli@unesp.br>
2023-10-26 22:33:40 -0300

    Inserindo comentários e atualizando interpretador de comandos

--- main.s
+++ main.s
@@ -11,37 +11,45 @@ _start:
     movia r5, CONTROL_REGISTER
     movia r6, TEXT_STRING
     movia r12, BUFFER
+    movia r13, 0xA # ascii enter
 
 LOOP:
     ldb r11, 0(r6)
-    beq r11, zero, POOLING_LEITURA /* a string é terminada em nulo */
+    beq r11, zero, POLLING_LEITURA /* a string é terminada em nulo */
 
-    POOLING_ESCRITA:
+    # Escrever "Entre com o comando: " no terminal
+    POLLING_ESCRITA:
         ldwio r7, 0(r5)
         andhi r10, r7, MASK_WSPACE
-        beq r10, r0, POOLING_ESCRITA
+        beq r10, r0, POLLING_ESCRITA
         stwio r11, 0(r4)
         addi r6, r6, 1
 
     br LOOP
 
-POOLING_LEITURA:
+POLLING_LEITURA:
+    # Verifica se algum caractere foi enviado
     ldwio r7, 0(r4)
     andi r8, r7, MASK_RVALID
     andi r9, r7, MASK_DATA
+    beq r8, r0, POLLING_LEITURA # loop enquanto não há caractere 
 
-    beq r8, r0, POOLING_LEITURA
+    beq r9, r13, INTERPRETADOR_DE_COMANDOS # Verifica se enter: fim do buffer
+    stb r9, 0(r12) # Armazena no buffer o caracter
+    addi r12, r12, 1 # Avança no buffer[++i]
 
-    stb r9, 0(r12)
-    addi r12, r12, 1
-
-POOLING_TECLADO:
+# Escrever o input do teclado no terminal
+POLLING_TECLADO:
     ldwio r7, 0(r5)
     andhi r10, r7, MASK_WSPACE
-    beq r10, r0, POOLING_TECLADO
+    beq r10, r0, POLLING_TECLADO
     stwio r9, 0(r4)
-    br POOLING_LEITURA
+    br POLLING_LEITURA
 
+INTERPRETADOR_DE_COMANDOS:
+    # movia r12, BUFFER Limpar buffer (NO FINAL!!!!!)
+    br POLLING_LEITURA
+    
 TEXT_STRING:
 .asciz "\nEntre com o comando: "
 
